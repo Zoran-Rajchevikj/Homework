4e673cdfbdaab8c1e0ee21362276e4affe0ab3d1
Zoran-Rajchevikj <163005406+Zoran-Rajchevikj@users.noreply.github.com>
2024-03-11 17:02:58 +0100

    Add files via upload

---
+++ Task2-2.js
@@ -0,0 +1,8 @@
+const fs = require("fs");
+
+function sayHello(){
+    console.log("Hello World");
+}
+module.exports={
+    sayHello,
+}
---
+++ Task2.js
@@ -0,0 +1,2 @@
+const task = require("./Task2-2");
+task.sayHello();
