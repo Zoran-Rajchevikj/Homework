3f856fbb3fb48038be0d61085a654581ae25aed7
Zoran-Rajchevikj <163005406+Zoran-Rajchevikj@users.noreply.github.com>
2024-03-13 14:00:20 +0100

    Update Task2-2.js

--- Task2-2.js
+++ Task2-2.js
@@ -1,5 +1,5 @@
 // 2. make a function in another file (Simple hello world) and use it in another file -> local module
-const fs = require("fs");
+
 
 function sayHello(){
     console.log("Hello World");
