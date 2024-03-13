9de07ff74b61cf4afd2c621a31ad6b1414bee1d1
Zoran-Rajchevikj <163005406+Zoran-Rajchevikj@users.noreply.github.com>
2024-03-13 14:25:21 +0100

    rename

--- Task2-2.js
+++
@@ -1,9 +0,0 @@
-// 2. make a function in another file (Simple hello world) and use it in another file -> local module
-
-
-function sayHello(){
-    console.log("Hello World");
-}
-module.exports={
-    sayHello,
-}
--- Task2.js
+++
@@ -1,3 +0,0 @@
-// 2. make a function in another file (Simple hello world) and use it in another file -> local module
-const task = require("./Task2-2");
-task.sayHello();
